f382e9607a12917169dbab8bce5bb18fbd3d2b71
Tridib11 <tridibghosh12345@gmail.com>
2024-06-05 13:49:48 +0530

    files added

---
+++ week-1/Arrays.js
@@ -0,0 +1,6 @@
+const initialArray=[1,2,3,4,5];
+//initialArray.push(6) pushes at the last
+// initialArray.pop() pops from the last
+// initialArray.shift() pops from the front
+// initialArray.unshift(9) adds something to the front of the array
+console.log(initialArray);
