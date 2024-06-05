1e332207a263c3d1718a96b9dacf3450db9acb99
Tridib11 <tridibghosh12345@gmail.com>
2024-06-05 14:21:49 +0530

    files added

--- week-1-Javacript/Class.js
+++ week-1-Javacript/Class.js
@@ -8,10 +8,15 @@ class Animal {
   speak() {
     console.log(this.name + " speaks " + this.speaks);
   }
+
+  legs(){
+      console.log(this.legCount)
+  }
 }
 
 let dog = new Animal("dog", 4, "bhow bhow");
 let cat = new Animal("cat", 4, "meow meow");
 
 dog.speak();
+dog.legs()
 cat.speak();
