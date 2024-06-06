8f6deb0be5d55b94eefa90f0f9ce9790637e690b
Tridib11 <tridibghosh12345@gmail.com>
2024-06-06 17:10:27 +0530

    refactor: Update file access method and remove unnecessary code
    This commit refactors the file access method in `FileAccess.js` to use `fs.readFileSync` instead of `fs.readFile`. The previous method used a callback function, which is unnecessary for synchronous file reading. Additionally, unnecessary code for error handling and a loop has been removed. This improves the efficiency and readability of the code.

--- week-1-Javacript/FileAccess.js
+++ week-1-Javacript/FileAccess.js
@@ -1,9 +1,33 @@
+// const fs = require("fs");
+
+// fs.readFileSync("lol.txt", "utf-8", function(err, data) {
+//   if (err) {
+//     console.error("An error occurred while reading the file:", err);
+//     return;
+//   }
+//   console.log(data);
+// });
+
+// let ans=0;
+// for(let i=0;i<10000;i++){
+//   ans+=i;
+// }
+
+// console.log(ans)
+
+
 const fs = require("fs");
 
-fs.readFile("lol.txt", "utf-8", function(err, data) {
-  if (err) {
-    console.error("An error occurred while reading the file:", err);
-    return;
-  }
+try {
+  const data = fs.readFileSync("lol.txt", "utf-8");
   console.log(data);
-});
+} catch (err) {
+  console.error("An error occurred while reading the file:", err);
+}
+
+let ans = 0;
+for (let i = 0; i < 10000; i++) {
+  ans += i;
+}
+
+console.log(ans);
