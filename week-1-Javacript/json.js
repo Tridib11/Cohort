b6f9960ccd4e96b2fa533185a47283529af3f1b9
Tridib11 <tridibghosh12345@gmail.com>
2024-06-06 16:52:59 +0530

    feat: Add JSON parsing and stringifying functionality
    This commit adds the ability to parse a JSON string into a JavaScript object and stringify a JavaScript object into a JSON string. The `json.js` file contains the implementation for this functionality. The `users` object is defined and then converted to a JSON string using `JSON.stringify()`. The resulting JSON string is logged to the console.

---
+++ week-1-Javacript/json.js
@@ -0,0 +1,11 @@
+//const users='{"name":"Tridib","age":21,"gender":"male"}';
+//const user=JSON.parse(users)//used to convert string to javascript object or json
+
+const users={ name: 'Tridib', age: 21, gender: 'male' }
+
+const user=JSON.stringify(users) //converts 
+
+
+
+
+console.log(user)
