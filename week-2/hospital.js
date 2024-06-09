716367b57d05871e6c233ab5ad0d066438cb1b01
Tridib11 <tridibghosh12345@gmail.com>
2024-06-09 08:18:10 +0530

    feat: Add endpoint to retrieve kidney health statistics

---
+++ week-2/hospital.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const app = express();
+
+const users = [{
+  name: "John",
+  kidneys: [
+    { healthy: false },
+    { healthy: true },
+    { healthy: true }
+  ]
+}];
+
+app.get("/", (req, res) => {
+  const johnKidneys = users[0].kidneys;
+  const numberOfKidneys = johnKidneys.length;
+  let numberOfHealthyKidneys = 0;
+
+  for (let i = 0; i < numberOfKidneys; i++) {
+    if (johnKidneys[i].healthy) {
+      numberOfHealthyKidneys++;
+    }
+  }
+
+  const numberOfUnhealthyKidneys=numberOfKidneys-numberOfHealthyKidneys
+
+  res.json({
+    numberOfKidneys,
+    numberOfHealthyKidneys,
+    numberOfUnhealthyKidneys
+  })
+});
+
+app.listen(3000, () => {
+  console.log("Server started");
+});
