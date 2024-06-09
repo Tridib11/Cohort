646d275ca08c9523be21ecc6f157100e1fc9ddb0
Tridib11 <tridibghosh12345@gmail.com>
2024-06-09 13:59:05 +0530

    chore: Update npm dependencies and add new files

--- week-3/rateLimiter.js
+++ week-3/rateLimiter.js
@@ -1,19 +1,36 @@
-const express=require("express")
+const express = require("express");
 
-const app=express()
+const app = express();
 
-let limiter=0;
-function rateLimiter(req,res,next){
+let limiter = 0;
+let totalResponseTime = 0;
+let requestCount = 0;
+
+function rateLimiter(req, res, next) {
   limiter++;
-  if(limiter<=5){
+  if (limiter <= 5) {
     next();
   }
 }
-app.get("/",rateLimiter,(req,res)=>{
-  res.status(201).send("Hey from rate limiter")
-})
 
+function measureResponseTime(req, res, next) {
+  const start = Date.now();
+  res.on("finish", () => {
+    const duration = Date.now() - start;
+    totalResponseTime += duration;
+    requestCount++;
+    const averageResponseTime = totalResponseTime / requestCount;
+    console.log(`Average Response Time: ${averageResponseTime.toFixed(2)} ms`);
+  });
+  next();
+}
 
-app.listen(3000,()=>{
-  console.log("Server started at port 3000")
-})
+app.use(measureResponseTime);
+
+app.get("/", rateLimiter, (req, res) => {
+  res.status(201).send("Hey from rate limiter");
+});
+
+app.listen(3000, () => {
+  console.log("Server started at port 3000");
+});
