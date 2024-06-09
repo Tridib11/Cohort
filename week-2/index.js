75cf7b139954bfd1ccff112b63109bb7cec91eb9
Tridib11 <tridibghosh12345@gmail.com>
2024-06-09 07:14:55 +0530

    chore: Add body-parser middleware for JSON parsing

--- week-2/index.js
+++ week-2/index.js
@@ -1,8 +1,14 @@
 const express=require("express")
+const bodyParser=require("body-parser")
 const app=express()
 
+app.use(bodyParser.json())
 app.get("/",(req,res)=>{
   res.send("Hello world")
+})
+
+app.post("/posting",(req,res)=>{
+  console.log(req.body)
 })
 
 app.listen(3000,()=>{
