99fcdc18e62da979961081d8797f4cbc0ec456b8
Tridib11 <tridibghosh12345@gmail.com>
2024-06-09 10:58:49 +0530

    chore: Update npm dependencies and add new files

--- week-3/rateLimiter.js
+++ week-3/rateLimiter.js
@@ -2,3 +2,18 @@ const express=require("express")
 
 const app=express()
 
+let limiter=0;
+function rateLimiter(req,res,next){
+  limiter++;
+  if(limiter<=5){
+    next();
+  }
+}
+app.get("/",rateLimiter,(req,res)=>{
+  res.status(201).send("Hey from rate limiter")
+})
+
+
+app.listen(3000,()=>{
+  console.log("Server started at port 3000")
+})
