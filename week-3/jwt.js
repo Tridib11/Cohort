5de16e74bd0da8dfba770f4a7e3c2fe220018f34
Tridib11 <tridibghosh12345@gmail.com>
2024-06-10 13:54:45 +0530

    feat: Add JWT authentication functionality

---
+++ week-3/jwt.js
@@ -0,0 +1,20 @@
+const express=require("express")
+const app=express()
+
+const allUsers=[
+  {
+    username:"tridib@mail.com",
+    password:"123",
+    name:"tridib"
+  },
+  {
+    username:"rohan@mail.com",
+    password:"456",
+    name:"rohan"
+  },
+  {
+    username:"rahul@mail.com",
+    password:"789",
+    name:"rahul"
+  }
+]
