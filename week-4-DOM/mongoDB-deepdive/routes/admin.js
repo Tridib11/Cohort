6ff4f0aadaf2737ace8f4684162e585ed3d73b56
Tridib11 <tridibghosh12345@gmail.com>
2024-06-17 00:21:17 +0530

    FIlle added

--- week-4-DOM/mongoDB-deepdive/routes/admin.js
+++ week-4-DOM/mongoDB-deepdive/routes/admin.js
@@ -1,4 +1,4 @@
-const { Router } = require("express");
+const { Router, response } = require("express");
 const adminMiddleware = require("../middleware/admin.js");
 const zod = require("zod");
 const { Admin, Course } = require("../db/index.js");
@@ -50,7 +50,7 @@ router.post("/signup", async (req, res) => {
     });
   }
 });
-router.post("/courses", adminMiddleware, (req, res) => {
+router.post("/courses", adminMiddleware, async(req, res) => {
   // Implement course creation logic
 
   const title=req.body.title
@@ -58,16 +58,23 @@ router.post("/courses", adminMiddleware, (req, res) => {
   const imageLink=req.body.imageLink
   const price=req.body.price
 
-  Course.create({
+  const newCourse=await Course.create({
     title,
     description,
     imageLink,
     price
   })
+  res.json({
+    message:'Course created Successfully ',courseId:newCourse._id
+  })
 });
 
-router.get("/courses", adminMiddleware, (req, res) => {
+router.get("/courses", adminMiddleware, async(req, res) => {
   // Implement fetching all courses logic
+    const allCourses=await Course.find({})
+    res.json({
+        allCourses
+    })
 });
 
 module.exports = router;
--- week-4-DOM/mongoDB-deepdive/routes/user.js
+++ week-4-DOM/mongoDB-deepdive/routes/user.js
@@ -1,23 +1,69 @@
 const { Router } = require("express");
 const router = Router();
+const zod = require("zod");
 const userMiddleware = require("../middleware/user");
+const { User,Course } = require("../db");
 
+const userSignupSchema = zod.object({
+  username: zod.string(),
+  password: zod.string().min(4),
+});
 
 // User Routes
-router.post('/signup', (req, res) => {
-    // Implement user signup logic
+router.post("/signup", async (req, res) => {
+  // Implement user signup logic
+
+  const validationResult = userSignupSchema.safeParse(req.body);
+
+  if (!validationResult.success) {
+    res.status(400).json({
+      msg: "Invalid request data",
+      errors: validationResult.error.issues,
+    });
+    return;
+  }
+
+  const { username, password } = validationResult.data;
+
+  try {
+    const existingUser = await User.findOne({ username });
+
+    if (existingUser) {
+      res.status(403).json({
+        msg: "User already exist",
+      });
+    } else {
+      const User = await User.create({
+        username,
+        password,
+      });
+
+      res.status(201).json({
+        msg: "User created Successfully",
+      });
+    }
+  } catch (err) {
+    res.status(500).json({
+      msg: "Error creating User",
+      error: err.message,
+    });
+  }
 });
 
-router.get('/courses', (req, res) => {
-    // Implement listing all courses logic
+router.get("/courses", async(req, res) => {
+  // Implement listing all courses logic
+  const allCourses=await Course.find({})
+    res.json({
+        allCourses
+    })
 });
 
-router.post('/courses/:courseId', userMiddleware, (req, res) => {
-    // Implement course purchase logic
+router.post("/courses/:courseId", userMiddleware, (req, res) => {
+  // Implement course purchase logic
 });
 
-router.get('/purchasedCourses', userMiddleware, (req, res) => {
-    // Implement fetching purchased courses logic
+router.get("/purchasedCourses", userMiddleware, (req, res) => {
+  // Implement fetching purchased courses logic
 });
 
-module.exports = router
+module.exports = router;
