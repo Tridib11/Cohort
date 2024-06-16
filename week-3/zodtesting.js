c8b00a5d30b5b79320f2be6f7faf576c733721be
Tridib11 <tridibghosh12345@gmail.com>
2024-06-17 00:21:29 +0530

    files added

--- week-3/zodtesting.js
+++ week-3/zodtesting.js
@@ -1,5 +1,5 @@
 const zod = require("zod");
-function validateInput(obj) {
+function validateInput( obj) {
   const schema = zod.object({
     email: zod.string().email(),
     password: zod.string().min(8),
