96222e6d5c5b5a8e5658feba7eb3db77ef48e369
Tridib11 <tridibghosh12345@gmail.com>
2024-06-19 22:41:45 +0530

    feat: Update backend API to handle todo creation, retrieval, and completion

--- week-5-react/Todo-app/backend/index.js
+++ week-5-react/Todo-app/backend/index.js
@@ -1,36 +1,66 @@
-const express=require("express")
-const {createTodo}=require("./types")
-const app=express()
+const express = require("express");
+const { createTodo } = require("./types");
+const todo = require("./db");
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-app.post("/todo",(req,res)=>{
-  const createPayload=req.body
-  const parsePayload=createPayload.safeparse(createPayload)
-  if(!parsePayload.success){
+app.post("/todo", async (req, res) => {
+  const createPayload = req.body;
+  const parsePayload = createPayload.safeparse(createPayload);
+  if (!parsePayload.success) {
     res.status(411).json({
-      msg:"Your inputs are wrong"
-    })
-    return ;
+      msg: "Your inputs are wrong",
+    });
+    return;
   }
-})
 
-app.get("/todos",(req,res)=>{
+  //put it in mongodb
+
+  await todo.create({
+    title: createPayload.title,
+    description: createPayload.description,
+    completed: false,
+  });
+
+  res.json({
+    msg: "Todo created",
+  });
+});
+
+app.get("/todos", async (req, res) => {
+  const todos = await todo.find({});
+  res.json({
+    todos,
+  });
+});
+
+app.put("/completed", async (req, res) => {
+  const createPayload = req.body;
+  const parsePayload = createPayload.safeparse(createPayload);
+  if (!parsePayload.success) {
+    res.status(411).json({
+      msg: "Your inputs are wrong",
+    });
+    return;
+  }
+
+  await todo.update(
+    {
+      _id: req.body.id,
+    },
+    {
+      completed: true,
+    }
+  )
+  res.json({
+    msg:"Todo marked as completed"
+  })
   
-})
+  
+  ;
+});
 
-app.put("/completed",(req,res)=>{
-  const createPayload=req.body
-  const parsePayload=createPayload.safeparse(createPayload)
-  if(!parsePayload.success){
-    res.status(411).json({
-      msg:"Your inputs are wrong"
-    })
-    return ;
-  }
-})
-
-
-app.listen(3000,()=>{
-  console.log("Server started")
-})
+app.listen(3000, () => {
+  console.log("Server started");
+});
