be8c1fda148f2812bacbf20b56269bac446dcffd
Tridib11 <tridibghosh12345@gmail.com>
2024-06-19 14:16:41 +0530

    feat: Add functionality to add a random todo in Todo component

--- week-5-react/counter-app/src/Todo.jsx
+++ week-5-react/counter-app/src/Todo.jsx
@@ -1,20 +1,29 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 function Todo() {
-  const [todos,setTodos]=useState([
+  const [todos, setTodos] = useState([
     {
-      id:1,
-      title:"Go to gym",
-      description:"Gym from 8-9"
+      id: 1,
+      title: "Go to gym",
+      description: "Gym from 8-9",
     },
     {
-      id:2,
-      title:"DSA",
-      description:"DSA from 6-8"
-    }
-  ])
+      id: 2,
+      title: "DSA",
+      description: "DSA from 6-8",
+    },
+  ]);
+
+  function AddTodo(){
+    setTodos([...todos,{
+        title:"New Todo",
+        description:"Desc of new todo"
+    }])
+  }
   return (
     <div>
+
+      <button onClick={AddTodo}>Add a random todo</button>
       {/* {JSON.stringify(todos)} */}
 
       {/* <Todos title="Go to gym" description="Gym from 8-9"/>
@@ -24,20 +33,20 @@ function Todo() {
       <Todos title="Go to gym" description="Gym from 8-9"/>
       <Todos title="Go to gym" description="Gym from 8-9"/> */}
 
-      {todos.map(function(todo){
-        return <Todos title={todo.title} description={todo.description}/>
+      {todos.map(function (todo) {
+        return <Todos title={todo.title} description={todo.description} />;
       })}
     </div>
-  )
+  );
 }
 
-
-
-function Todos(props){
-  return <>
-  <h1>{props.title}</h1>
-  <h2>{props.description}</h2>
-  </>
+function Todos(props) {
+  return (
+    <>
+      <h1>{props.title}</h1>
+      <h2>{props.description}</h2>
+    </>
+  );
 }
 
-export default Todo
+export default Todo;
