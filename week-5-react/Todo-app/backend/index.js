99759c896c00cdab5937c3919f20f362834b25fd
Tridib11 <tridibghosh12345@gmail.com>
2024-06-19 23:52:17 +0530

    feat: Add cors dependency for handling cross-origin resource sharing

--- week-5-react/Todo-app/backend/index.js
+++ week-5-react/Todo-app/backend/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const { createTodo } = require("./types");
 const todo = require("./db");
+const {cors}=require("cors")
 const app = express();
 
+app.use(cors())
 app.use(express.json());
 
 app.post("/todo", async (req, res) => {
