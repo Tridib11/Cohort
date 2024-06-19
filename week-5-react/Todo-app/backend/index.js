d82b0457f533b8fb99ed2a8944eabfe2a237aca7
Tridib11 <tridibghosh12345@gmail.com>
2024-06-20 00:24:55 +0530

    chore: Update cors import in backend index.js

--- week-5-react/Todo-app/backend/index.js
+++ week-5-react/Todo-app/backend/index.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const { createTodo } = require("./types");
 const todo = require("./db");
-const {cors}=require("cors")
+const cors = require("cors")
 const app = express();
 
-app.use(cors())
 app.use(express.json());
+app.use(cors())
 
 app.post("/todo", async (req, res) => {
   const createPayload = req.body;
