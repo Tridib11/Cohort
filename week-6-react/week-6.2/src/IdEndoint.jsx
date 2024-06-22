ba0f4d793a8b549020052e5af4227dcf48bd6057
Tridib11 <tridibghosh12345@gmail.com>
2024-06-22 23:59:50 +0530

    feat: Add IdEndpoint component for handling specific ID endpoint

---
+++ week-6-react/week-6.2/src/IdEndoint.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function IdEndoint() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default IdEndoint
--- week-6-react/week-6.2/src/main.jsx
+++ week-6-react/week-6.2/src/main.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+// import App from './App.jsx'
+import IdEndoint from './IdEndoint.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <IdEndoint/>
   </React.StrictMode>,
 )
