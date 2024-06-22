4d5e66c75d4cbff4f0b2157766f93f92336b64a0
Tridib11 <tridibghosh12345@gmail.com>
2024-06-23 00:32:42 +0530

    feat: Add lazy loading for images in ButtonCLicker component

--- week-6-react/week-6.2/src/ButtonCLicker.jsx
+++ week-6-react/week-6.2/src/ButtonCLicker.jsx
@@ -1,20 +1,42 @@
 import React, { useEffect, useState } from 'react'
-
+import axios from 'axios'
 function ButtonCLicker() {
+  const[id,setId]=useState(1)
   return (
     <div>
-      
+      <button onClick={()=>{
+        setId(1)
+      }} >1</button>
+      <button onClick={()=>{
+        setId(2)
+      }} >2</button>
+      <button onClick={()=>{
+        setId(3)
+      }} >3</button>
+      <button onClick={()=>{
+        setId(4)
+      }} >4</button>
+      <Todo id={id}/>
     </div>
   )
 }
 
 
 
-function Todo(){
+function Todo({id}){
   const[todos,setTodos]=useState([])
   useEffect(()=>{
-    
-  },[])
+    axios.get("https://sum-server.100xdevs.com/todo?id="+id)
+    .then((response)=>{
+      setTodos(response.data.todo)
+    })
+  },[id])
+
+
+  return <div>
+    <h1>{todos.title}</h1> <br />
+    <h3>{todos.description}</h3>
+  </div>
 }
 
 export default ButtonCLicker
