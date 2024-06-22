df5e1f445903c62c028bb8421d52255255ea6f0c
Tridib11 <tridibghosh12345@gmail.com>
2024-06-22 23:12:12 +0530

    feat: Update App.jsx to fetch and display todos from API

--- week-6-react/week-6.2/src/App.jsx
+++ week-6-react/week-6.2/src/App.jsx
@@ -1,14 +1,37 @@
-import { useState } from 'react'
+import axios from "axios"
+import { useEffect, useState } from "react"
+
 
 
 function App() {
-  const [count, setCount] = useState(0)
+ const[todos,setTodos]=useState([])
+
+ useEffect(()=>{
+  axios.get("https://sum-server.100xdevs.com/todos")
+  .then(function(response){
+    setTodos(response.data.todos)
+  })
+ },[])
 
   return (
     <>
-      
+      {todos.map(todo=><Todo title={todo.title} description={todo.description}/>)}
     </>
   )
+}
+
+
+function Todo({title,description}){
+  return <div>
+    <h1>
+
+    {title}
+    </h1>
+    <h3>
+
+    {description}
+    </h3>
+  </div>
 }
 
 export default App
