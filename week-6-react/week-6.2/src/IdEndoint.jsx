e6c2ad0abefcdc946c1a97a3641d2ce3d9f93d3c
Tridib11 <tridibghosh12345@gmail.com>
2024-06-23 00:17:25 +0530

    feat: Add lazy loading for images in IdEndpoint component

--- week-6-react/week-6.2/src/IdEndoint.jsx
+++ week-6-react/week-6.2/src/IdEndoint.jsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
-function IdEndoint() {
-  return (
-    <div>
-      
+
+function IdEndoint() { 
+  return <div>
+      <Todo id={3}/>
     </div>
-  )
+  
 }
+
+
+function Todo({id}){
+  const [todo,setTodos]=useState([])
+  useEffect(()=>{
+    axios.get("https://sum-server.100xdevs.com/todo?id="+id)
+    .then(function(response){
+      setTodos(response.data.todo)
+    })
+  },[])
+
+
+  return <div>
+    <h1>
+
+    {todo.title}
+    </h1>
+    <h3>
+
+      {todo.description}
+    </h3>
+  </div>
+}
+
 
 export default IdEndoint
