52f87f3df7f2670d319e9a8e82cfac8830a97ee5
Tridib11 <tridibghosh12345@gmail.com>
2024-06-24 21:29:07 +0530

    feat: Optimize filtering process in Assignment2 component using useMemo hook

--- week-6-react/week-6-Assignments/src/main.jsx
+++ week-6-react/week-6-Assignments/src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { Assignment2 } from './Assignment2.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Assignment2/>
   </React.StrictMode>,
 )
