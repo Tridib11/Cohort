539897b10cc74084f80f3cd5a7bde3b5835bab04
Tridib11 <tridibghosh12345@gmail.com>
2024-06-24 11:12:48 +0530

    feat: Optimize Memo component by using useMemo and useEffect hooks

--- week-6-react/week-6.2/src/Memo.jsx
+++ week-6-react/week-6.2/src/Memo.jsx
@@ -3,25 +3,26 @@ import React, { useEffect, useMemo, useState } from 'react';
 function Memo() {
   const [input, setInput] = useState(0);
   const [sum, setSum] = useState(0);
+  const [counter,setCounter]=useState(0);
 
-  // useEffect(() => {
-  //   console.log("rendered")
-  //   let sum = 0;
-  //   for (let i = 1; i <=input; i++) {
-  //     sum += i;
-  //   }
-  //   setSum(sum);
-  // }, [input]);
-
-
-  let count=useMemo(()=>{
+  useEffect(() => {
     console.log("rendered")
-    let count=0;
-    for(let i=0;i<=input;i++){
-      count+=i
+    let sum = 0;
+    for (let i = 1; i <=input; i++) {
+      sum += i;
     }
-    return count;
-  },[input])
+    setSum(sum);
+  }, [input]);
+
+
+  // let count=useMemo(()=>{
+  //   console.log("rendered")
+  //   let count=0;
+  //   for(let i=0;i<=input;i++){
+  //     count+=i
+  //   }
+  //   return count;
+  // },[input])
 
   return (
     <div>
@@ -29,7 +30,11 @@ function Memo() {
         onChange={(e) => setInput(e.target.value)}
         type="number"
       />
-      <h1>The value is {count}</h1>
+      <h1>The value is {sum}</h1>
+      <br />
+      <button onClick={()=>{
+        setCounter(counter+1)
+      }}>Count is {counter}</button>
     </div>
   );
 }
