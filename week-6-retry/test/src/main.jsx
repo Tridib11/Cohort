191ffe519c5ce7ffb3107f9aab2070d7536425d5
Tridib11 <tridibghosh12345@gmail.com>
2024-07-02 13:04:04 +0530

    chore: Add Todo component to main.jsx

--- week-6-retry/test/src/main.jsx
+++ week-6-retry/test/src/main.jsx
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import Test from './Test.jsx'
+import Todo from './Todo.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <Test/>
+    // <Test/>
+    <Todo/>
 )
