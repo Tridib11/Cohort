0e3aa0d4956d4ba669f2ad73d449fd08c7eafa3a
Tridib11 <tridibghosh12345@gmail.com>
2024-07-02 13:08:54 +0530

    feat: Add todo list functionality to Todo component

--- week-6-retry/test/src/Todo.jsx
+++ week-6-retry/test/src/Todo.jsx
@@ -1,11 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Todo() {
+
+  const[todo,setTodo]=useState([
+
+    {
+      id:1,
+      title:"Gym",
+      description:"Go to gym 8-9"
+    },
+    {
+      id:2,
+      title:"DSA",
+      description:"Study DSA"
+    },
+    {
+      id:3,
+      title:"eat",
+      description:"Eat food"
+    }
+  ]
+  )
   return (
     <div>
-      
+      {todo.map(todos=><DisplayTodo title={todos.title} description={todos.description}/>)}
     </div>
   )
+}
+
+
+function DisplayTodo({title,description}){
+  return <div>
+    <h1>
+      {title}
+    </h1>
+
+    <h2>
+      {description}
+    </h2>
+  </div>
 }
 
 export default Todo
