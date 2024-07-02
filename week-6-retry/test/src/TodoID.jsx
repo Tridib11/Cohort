c69654a714eb5360b25fa791b4feea013ddfec29
Tridib11 <tridibghosh12345@gmail.com>
2024-07-02 15:00:55 +0530

    feat: Add lazy loading for images in TodoID component

--- week-6-retry/test/src/TodoID.jsx
+++ week-6-retry/test/src/TodoID.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 function TodoID() {
+  const [todo, setTodo] = useState(null)
+  const [todoID, setTodoID] = useState('')
+
+  useEffect(() => {
+    if (todoID !== '') {
+      axios.get(`https://sum-server.100xdevs.com/todo?id=${todoID}`)
+        .then((response) => {
+          setTodo(response.data.todo)
+        })
+    }
+  }, [todoID])
+
   return (
     <div>
-      
+      <input
+        onChange={(e) => {
+          setTodoID(e.target.value)
+        }}
+        type="text"
+        placeholder='Enter the Todo ID'
+      />
+
+      {todo ? (
+        <DisplayTodo title={todo.title} description={todo.description} />
+      ) : (
+        <p>No todo found for the given ID.</p>
+      )}
+    </div>
+  )
+}
+
+function DisplayTodo({ title, description }) {
+  return (
+    <div>
+      <h1>{title}</h1>
+      <h3>{description}</h3>
     </div>
   )
 }
--- week-6-retry/test/src/main.jsx
+++ week-6-retry/test/src/main.jsx
@@ -5,9 +5,11 @@ import './index.css'
 import Test from './Test.jsx'
 import Todo from './Todo.jsx'
 import Todos from './Todos.jsx'
+import TodoID from './TodoID.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     // <Test/>
     // <Todo/>
-    <Todos/>
+    // <Todos/>
+    <TodoID/>
 )
