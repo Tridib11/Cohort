db2be07781e28b7df4684f0a2340b28415f6dfa2
Tridib11 <tridibghosh12345@gmail.com>
2024-07-02 12:19:20 +0530

    chore: Update App component with Header and title prop

--- week-6-retry/test/src/App.jsx
+++ week-6-retry/test/src/App.jsx
@@ -3,9 +3,16 @@ import React from 'react'
 function App() {
   return (
     <div>
-      
+      <Header title="tridib"/>
     </div>
   )
+}
+
+
+function Header({title}){
+  return <div>
+    {title}
+  </div>
 }
 
 export default App
