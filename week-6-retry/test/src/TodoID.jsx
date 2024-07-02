7534029133138c247aa058fcc79a47dc54cabef0
Tridib11 <tridibghosh12345@gmail.com>
2024-07-02 15:10:34 +0530

    feat: Add buttons for selecting todo ID in TodoID component

--- week-6-retry/test/src/TodoID.jsx
+++ week-6-retry/test/src/TodoID.jsx
@@ -16,6 +16,23 @@ function TodoID() {
 
   return (
     <div>
+      <button onClick={()=>{
+        setTodoID(1)
+      }}>1</button>
+      <button onClick={()=>{
+        setTodoID(2)
+      }}>2</button>
+      <button onClick={()=>{
+        setTodoID(3)
+      }}>3</button>
+      <button onClick={()=>{
+        setTodoID(4)
+      }}>4</button>
+      <button onClick={()=>{
+        setTodoID(5)
+      }}>5</button>
+      <br />
+      <br /><br />
       <input
         onChange={(e) => {
           setTodoID(e.target.value)
