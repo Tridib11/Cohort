4a3289dfe11e28c2baa055fb515a41d2da861104
Tridib11 <tridibghosh12345@gmail.com>
2024-07-02 15:14:28 +0530

    feat: Refactor TodoID component to use axios for fetching todo data

--- week-6-retry/test/src/TodoID.jsx
+++ week-6-retry/test/src/TodoID.jsx
@@ -1,42 +1,64 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 function TodoID() {
-  const [todo, setTodo] = useState({})
-  const [todoID, setTodoID] = useState(0)
+  const [todo, setTodo] = useState({});
+  const [todoID, setTodoID] = useState(0);
 
   useEffect(() => {
-      axios.get(`https://sum-server.100xdevs.com/todo?id=${todoID}`)
-        .then((response) => {
-          setTodo(response.data.todo)
-        })  
-  }, [todoID])
+    axios
+      .get(`https://sum-server.100xdevs.com/todo?id=${todoID}`)
+      .then((response) => {
+        setTodo(response.data.todo);
+      });
+  }, [todoID]);
 
   return (
     <div>
-      <button onClick={()=>{
-        setTodoID(1)
-      }}>1</button>
-      <button onClick={()=>{
-        setTodoID(2)
-      }}>2</button>
-      <button onClick={()=>{
-        setTodoID(3)
-      }}>3</button>
-      <button onClick={()=>{
-        setTodoID(4)
-      }}>4</button>
-      <button onClick={()=>{
-        setTodoID(5)
-      }}>5</button>
+      <button
+        onClick={() => {
+          setTodoID(1);
+        }}
+      >
+        1
+      </button>
+      <button
+        onClick={() => {
+          setTodoID(2);
+        }}
+      >
+        2
+      </button>
+      <button
+        onClick={() => {
+          setTodoID(3);
+        }}
+      >
+        3
+      </button>
+      <button
+        onClick={() => {
+          setTodoID(4);
+        }}
+      >
+        4
+      </button>
+      <button
+        onClick={() => {
+          setTodoID(5);
+        }}
+      >
+        5
+      </button>
       <br />
-      <br /><br />
+      <br />
+      <br />
       <input
         onChange={(e) => {
-          setTodoID(e.target.value)
+          setTodoID(e.target.value);
         }}
         type="text"
-        placeholder='Enter the Todo ID'
+        placeholder="Enter the Todo ID"
       />
 
       {todo ? (
@@ -45,7 +67,7 @@ function TodoID() {
         <p>No todo found for the given ID.</p>
       )}
     </div>
-  )
+  );
 }
 
 function DisplayTodo({ title, description }) {
@@ -54,7 +76,7 @@ function DisplayTodo({ title, description }) {
       <h1>{title}</h1>
       <h3>{description}</h3>
     </div>
-  )
+  );
 }
 
-export default TodoID
+export default TodoID;
