d32c500f5d0fe8c48c52dbcd31dd3e9eec34fe02
Tridib11 <tridibghosh12345@gmail.com>
2024-07-03 22:13:34 +0530

    chore: Update ContextApi component to implement count functionality with buttons

---
+++ week-7/7.1/src/ContextApi.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+
+function ContextApi() {
+  const[count,setCount]=useState(0)
+
+  return (
+    <div>
+      <Count count={count} setCount={setCount}/>
+    </div>
+  )
+}
+
+function Count({count,setCount}){
+  return <div>
+    {count}
+    <Buttons count={count} setCount={setCount}/>
+  </div>
+}
+
+function Buttons({count,setCount}){
+  return <div>
+    <button onClick={()=>{
+      setCount(count+1)
+    }}>
+      Increase
+    </button>
+
+    <button onClick={()=>{
+      setCount(count-1)
+    }}>
+      Decrease
+    </button>
+  </div>
+}
+
+export default ContextApi
--- week-7/7.1/src/main.jsx
+++ week-7/7.1/src/main.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+// import App from './App.jsx'
 import './index.css'
-import LazyLoading from './LazyLoading.jsx'
+import ContextApi from './ContextApi'
+// import LazyLoading from './LazyLoading.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   
-    <LazyLoading/>
+    // <LazyLoading/>
+    <ContextApi/>
 
 )
