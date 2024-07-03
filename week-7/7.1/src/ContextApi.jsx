9b8005faa35d1a9f457a82838b663c8b293b1fe0
Tridib11 <tridibghosh12345@gmail.com>
2024-07-03 22:50:19 +0530

    chore: Refactor ContextApi component to use CountContext for count functionality and buttons

--- week-7/7.1/src/ContextApi.jsx
+++ week-7/7.1/src/ContextApi.jsx
@@ -1,44 +1,49 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from "react";
+import { CountContext } from "./Context";
 
 function ContextApi() {
-  const[count,setCount]=useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div>
-      <Count count={count} setCount={setCount}/>
+      <CountContext.Provider value={count}>
+        <Count setCount={setCount} />
+      </CountContext.Provider>
     </div>
-  )
+  );
 }
 
-function Count({count,setCount}){
-  return <div>
-    <CountRenderer/>
-    <Buttons count={count} setCount={setCount}/>
-  </div>
+function Count({setCount}) {
+  return (
+    <div>
+      <CountRenderer />
+      <Buttons setCount={setCount} />
+    </div>
+  );
 }
 
-function CountRenderer(){
-  const count=1;
-  return <div>
-    {count}
-  </div>
+function CountRenderer() {
+  const count = useContext(CountContext);
+  return <div>{count}</div>;
 }
 
-function Buttons({setCount}){
-  const count=1
-  return <div>
-    <button onClick={()=>{
-      setCount(count+1)
-    }}>
-      Increase
-    </button>
+function Buttons({ setCount }) {
+  const count = useContext(CountContext);
+  return (
+    <div>
+      <button onClick={() => {
+          setCount(count + 1);
+        }}>
+        Increase
+      </button>
 
-    <button onClick={()=>{
-      setCount(count-1)
-    }}>
-      Decrease
-    </button>
-  </div>
+      <button onClick={() => {
+          setCount(count - 1);
+        }}>
+        Decrease
+      </button>
+    </div>
+  );
 }
 
-export default ContextApi
+export default ContextApi;
