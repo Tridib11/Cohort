628c4f59088038a544e992b01ba91310b7d36151
Tridib11 <tridibghosh12345@gmail.com>
2024-07-03 23:01:26 +0530

    chore: Wrap component with CountContext for count functionality and buttons

--- week-7/7.1/src/ContextApi.jsx
+++ week-7/7.1/src/ContextApi.jsx
@@ -1,3 +1,58 @@
+// import React, { useContext, useState } from "react";
+// import { CountContext } from "./Context";
+// //Wrap the component that want to use the teleported value inside the count context provider
+// function ContextApi() {
+//   const [count, setCount] = useState(0);
+
+//   return (
+//     <div>
+//       <CountContext.Provider value={count}>
+//         <Count setCount={setCount} />
+//       </CountContext.Provider>
+//     </div>
+//   );
+// }
+
+// function Count({setCount}) {
+//   return (
+//     <div>
+//       <CountRenderer />
+//       <Buttons setCount={setCount} />
+//     </div>
+//   );
+// }
+
+// function CountRenderer() {
+//   const count = useContext(CountContext);
+//   return <div>{count}</div>;
+// }
+
+// function Buttons({ setCount }) {
+//   const count = useContext(CountContext);
+//   return (
+//     <div>
+//       <button onClick={() => {
+//           setCount(count + 1);
+//         }}>
+//         Increase
+//       </button>
+
+//       <button onClick={() => {
+//           setCount(count - 1);
+//         }}>
+//         Decrease
+//       </button>
+//     </div>
+//   );
+// }
+
+// export default ContextApi;
+
+
+
+//A better way
+
+
 import React, { useContext, useState } from "react";
 import { CountContext } from "./Context";
 
@@ -6,40 +61,44 @@ function ContextApi() {
 
   return (
     <div>
-      <CountContext.Provider value={count}>
-        <Count setCount={setCount} />
+      <CountContext.Provider value={{ count, setCount }}>
+        <Count />
       </CountContext.Provider>
     </div>
   );
 }
 
-function Count({setCount}) {
+function Count() {
   return (
     <div>
       <CountRenderer />
-      <Buttons setCount={setCount} />
+      <Buttons />
     </div>
   );
 }
 
 function CountRenderer() {
-  const count = useContext(CountContext);
+  const { count } = useContext(CountContext);
   return <div>{count}</div>;
 }
 
-function Buttons({ setCount }) {
-  const count = useContext(CountContext);
+function Buttons() {
+  const { count, setCount } = useContext(CountContext);
   return (
     <div>
-      <button onClick={() => {
+      <button
+        onClick={() => {
           setCount(count + 1);
-        }}>
+        }}
+      >
         Increase
       </button>
 
-      <button onClick={() => {
+      <button
+        onClick={() => {
           setCount(count - 1);
-        }}>
+        }}
+      >
         Decrease
       </button>
     </div>
