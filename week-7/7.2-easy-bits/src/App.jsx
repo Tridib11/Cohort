6bcd25ac36b2527b67279719ba6a2289bce9fe9c
Tridib11 <tridibghosh12345@gmail.com>
2024-07-05 22:44:30 +0530

    chore: Add navigation buttons to App component

--- week-7/7.2-easy-bits/src/App.jsx
+++ week-7/7.2-easy-bits/src/App.jsx
@@ -3,7 +3,14 @@ import React from 'react'
 function App() {
   return (
     <div>
-      
+      <button>Home</button>
+
+      <button>My Networks ()</button>
+      <button>Jobs ()</button>
+      <button>Messaging ()</button>
+      <button>Notifications ()</button>
+
+      <button>Me</button>
     </div>
   )
 }
