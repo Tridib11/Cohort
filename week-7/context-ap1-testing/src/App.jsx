b41219b776ae0dc4000cd2a040acee6b03709abc
Tridib11 <tridibghosh12345@gmail.com>
2024-07-05 12:46:14 +0530

    File added

--- week-7/context-ap1-testing/src/App.jsx
+++ week-7/context-ap1-testing/src/App.jsx
@@ -6,19 +6,38 @@ function App() {
   return (
     <div>
       <CountContext.Provider value={count}>
-        <CountRender setCount={setCount}/>
+        <Count setCount={setCount} />
       </CountContext.Provider>
     </div>
   )
 }
 
-function CountRender({setCount}){
+function Count({setCount}){
+  console.log("rendered")
+  return <div>
+    <CountRender/>
+    <Buttons setCount={setCount}/>
+  </div>
+}
+
+function CountRender(){
   const count=useContext(CountContext)
   return <div>
-    {count} <br />
+    {count} 
+  </div>
+}
+
+function Buttons({setCount}){
+  const count=useContext(CountContext)
+  return <div>
+    <br />
     <button onClick={()=>{
       setCount(count+1)
-    }}>Click me</button>
+    }}>Increase</button>
+    <br />
+    <button onClick={()=>{
+      setCount(count-1)
+    }}>Decrease</button>
   </div>
 }
 
