ba1f0b03da47dc61439a523af22e60ca3dbb198a
Tridib11 <tridibghosh12345@gmail.com>
2024-07-08 22:59:59 +0530

    chore: Update Tailwind CSS configuration to include additional content files

--- week-8-tailwind/tailwind-prac/src/App.jsx
+++ week-8-tailwind/tailwind-prac/src/App.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 
 function App() {
   return (
-    <div>
-      
+    <div className='flex justify-center'>
+      <div className='bg-red-500'>Hi</div>
+      <div className='bg-green-500'>Hi</div>
+      <div className='bg-yellow-500'>Hi</div>
+      <div className='bg-blue-500'>Hi</div>
     </div>
   )
 }
-
 export default App
