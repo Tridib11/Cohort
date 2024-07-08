89bf9a4d852fd6277ea194ea17061247628821a4
Tridib11 <tridibghosh12345@gmail.com>
2024-07-08 23:06:53 +0530

    chore: Update grid layout in App.jsx to use 2 columns instead of 3

--- week-8-tailwind/tailwind-prac/src/App.jsx
+++ week-8-tailwind/tailwind-prac/src/App.jsx
@@ -12,17 +12,27 @@ function App() {
           
       </div>
       <Grids/>
+      <GridsSpan/>
       
     </div>
   )
 }
 
 function Grids(){
-  return <div className='grid grid-cols-3 mt-4'>
+  return <div className='grid grid-cols-2 mt-4'>
       <div className='bg-red-500'>Hi</div>
       <div className='bg-green-500'>Hi</div>
       <div className='bg-yellow-500'>Hi</div>
       <div className='bg-blue-500'>Hi</div>
+    </div>
+}
+
+function GridsSpan(){
+  return <div className='grid grid-cols-10 mt-4'>
+      <div className='bg-red-500 col-span-4'>Hi</div>
+      <div className='bg-green-500 col-span-4'>Hi</div>
+      <div className='bg-yellow-500 col-span-2'>Hi</div>
+      
     </div>
 }
 export default App
