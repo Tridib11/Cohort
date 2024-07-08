c1b98cc5c91ab9c2e583cfcdb1453f48b250e33c
Tridib11 <tridibghosh12345@gmail.com>
2024-07-08 23:03:48 +0530

    chore: Update Tailwind CSS configuration and add grid layout to App.jsx

--- week-8-tailwind/tailwind-prac/src/App.jsx
+++ week-8-tailwind/tailwind-prac/src/App.jsx
@@ -2,12 +2,27 @@ import React from 'react'
 
 function App() {
   return (
-    <div className='flex justify-center'>
+    <div>
+
+      <div className='flex justify-center'>
+        <div className='bg-red-500'>Hi</div>
+        <div className='bg-green-500'>Hi</div>
+        <div className='bg-yellow-500'>Hi</div>
+        <div className='bg-blue-500'>Hi</div>
+          
+      </div>
+      <Grids/>
+      
+    </div>
+  )
+}
+
+function Grids(){
+  return <div className='grid grid-cols-3 mt-4'>
       <div className='bg-red-500'>Hi</div>
       <div className='bg-green-500'>Hi</div>
       <div className='bg-yellow-500'>Hi</div>
       <div className='bg-blue-500'>Hi</div>
     </div>
-  )
 }
 export default App
