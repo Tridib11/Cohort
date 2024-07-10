ea17962c9f2c241f97c2769ff732b669e7e8e25d
Tridib11 <tridibghosh12345@gmail.com>
2024-07-11 00:05:53 +0530

    refactor: Update Axios and Fetch code to handle response data consistently

--- week-8-Axios-Fetch/Axios.js
+++ week-8-Axios-Fetch/Axios.js
@@ -1,8 +1,25 @@
 // import axios from "axios";
 const axios=require("axios")
 
-axios.get("https://sum-server.100xdevs.com/todos")
-.then(async (res)=>{
-  const ans=await res.data
-  console.log(ans)
-})
+// axios.get("https://sum-server.100xdevs.com/todos")
+// .then(async (res)=>{
+//   const ans=await res.data
+//   console.log(ans)
+// })
+
+
+async function main(){
+  const response =await axios.post("https://httpdump.app/dumps/79b56696-4879-4015-905d-b02f5dba4475",{
+    username:"Tridib",
+    password:"12334"
+  },{
+    headers:{
+      Authorization:"Bearer 123"
+    }
+  })
+
+  console.log(response.data)
+
+
+}
+main()
