eb80418f82cf45f932d74a784346e3952ae7872f
Tridib11 <tridibghosh12345@gmail.com>
2024-07-14 23:19:20 +0530

    chore: Update App component to include MyClassComponent

--- week-9/hooks-practice/src/App.jsx
+++ week-9/hooks-practice/src/App.jsx
@@ -1,10 +1,11 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import './App.css'
 
 function App() {
   return (
     <>
-     <MyConponent/>
+     {/* <MyConponent/> */}
+     <MyClassComponent/>
     </>
   )
 }
@@ -19,6 +20,28 @@ function MyConponent(){
       setCount(count=>count+1)
     }}>Click me</button>
   </div>
+}
+
+
+class MyClassComponent extends React.Component{
+  constructor(props){
+    super(props)
+    this.state={count:0}
+  }
+
+  incrementCount=()=>{
+    this.setState({count:this.state.count+1})
+  }
+
+
+  render(){
+    return(
+      <div>
+        <p>{this.state.count}</p>
+        <button onClick={this.incrementCount}>Increment</button>
+      </div>
+    )
+  }
 }
 
 export default App
