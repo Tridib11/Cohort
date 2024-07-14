b9993d1bb6a89cc7be3d9e99b1625c7dc06140bb
Tridib11 <tridibghosh12345@gmail.com>
2024-07-14 23:38:00 +0530

    chore: Update main.jsx to import NewApp instead of Testing

--- week-9/hooks-practice/src/main.jsx
+++ week-9/hooks-practice/src/main.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 // import App from './App.jsx'
 import './index.css'
-import Testing from './Testing'
-// import NewApp from './NewApp'
+// import Testing from './Testing'
+import NewApp from './NewApp'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
   //   <App />
   // </React.StrictMode>,
 
-  // <NewApp/>
-  <Testing/>
+  <NewApp/>
+  // <Testing/>
 )
