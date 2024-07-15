6c167d813f18b2f90430ddfe1d3372d0ae3d0415
Tridib11 <tridibghosh12345@gmail.com>
2024-07-15 22:49:37 +0530

    feat: Add sum function in JavaScript and TypeScript files

---
+++ week-9/Ts-Intro/node-app/sumfn.js
@@ -0,0 +1,4 @@
+function sum(a, b) {
+    console.log("The sum is ".concat(a + b));
+}
+sum(2, 3);
---
+++ week-9/Ts-Intro/node-app/sumfn.ts
@@ -0,0 +1,5 @@
+function sum(a:number,b:number){
+  console.log(`The sum is ${a+b}`)
+}
+
+sum(2,3)
