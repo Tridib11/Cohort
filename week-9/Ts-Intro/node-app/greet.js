8b56c979be38e91f46ba91a4b6daff6d7a6e9629
Tridib11 <tridibghosh12345@gmail.com>
2024-07-15 22:38:06 +0530

    Refactor variable declaration in a.js and greet.js files

--- week-9/Ts-Intro/node-app/greet.js
+++ week-9/Ts-Intro/node-app/greet.js
@@ -1,10 +1,9 @@
-"use strict";
 //In js
 // function Greet(name){
 //   console.log(`hello ${name}`)
 // }
 // Greet("Tridib")
 function Greet(name) {
-    console.log(`hello ${name}`);
+    console.log("hello ".concat(name));
 }
 Greet("Tridib");
