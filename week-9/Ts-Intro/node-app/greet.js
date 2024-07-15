4b0415e6878105d3c4a5955ae37cea0cbe8ff533
Tridib11 <tridibghosh12345@gmail.com>
2024-07-15 23:51:59 +0530

    Refactor greet.js and sumfn.js files

--- week-9/Ts-Intro/node-app/greet.js
+++
@@ -1,9 +0,0 @@
-//In js
-// function Greet(name){
-//   console.log(`hello ${name}`)
-// }
-// Greet("Tridib")
-function Greet(name, lastname, age) {
-    console.log("hello ".concat(name, " ").concat(lastname, " , age:").concat(age));
-}
-Greet("Tridib", "Ghosh", 12);
--- week-9/Ts-Intro/node-app/sumfn.js
+++
@@ -1,4 +0,0 @@
-function sum(a, b) {
-    console.log("The sum is ".concat(a + b));
-}
-sum(2, 3);
