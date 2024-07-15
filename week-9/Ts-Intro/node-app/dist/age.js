cb86c7f35262f888ba8d97cb417f38b23dae2fa4
Tridib11 <tridibghosh12345@gmail.com>
2024-07-15 23:51:36 +0530

    feat: Add fs, age, greet, and sum functions to Node.js application

---
+++ week-9/Ts-Intro/node-app/dist/age.js
@@ -0,0 +1,6 @@
+"use strict";
+function age(age) {
+    return age > 18 ? true : false;
+}
+const ans = age(21);
+console.log(ans);
---
+++ week-9/Ts-Intro/node-app/dist/greet.js
@@ -0,0 +1,10 @@
+"use strict";
+//In js
+// function Greet(name){
+//   console.log(`hello ${name}`)
+// }
+// Greet("Tridib")
+function Greet(name, lastname, age) {
+    console.log(`hello ${name} ${lastname} , age:${age}`);
+}
+Greet("Tridib", "Ghosh", 12);
