f4f4bd8d856346edc66392a34f932e3bdc83a3d8
Tridib11 <tridibghosh12345@gmail.com>
2024-07-16 01:50:47 +0530

    Add interfaces and types for class extension and intersection in intersection.ts

--- week-9/Ts-Intro/node-app/src/intersection.ts
+++ week-9/Ts-Intro/node-app/src/intersection.ts
@@ -1,5 +1,10 @@
 //What if you want to create a type that has every property of multiple types/ interfaces
 
+
+
+//Interfaces you can extend in a claass and types lets you do or,and ,union and intersections
+
+
 type employee={
   name:string
   startDate:Date
