a682274b08607a9b3fa98adcaf8730fe4a4b7c64
Tridib11 <tridibghosh12345@gmail.com>
2024-07-16 16:36:42 +0530

    feat: Add generic function for retrieving the first element of an array

--- week-9-TS-And-Hooks/Generics-enums-import-export/src/generics.ts
+++ week-9-TS-And-Hooks/Generics-enums-import-export/src/generics.ts
@@ -14,3 +14,13 @@ console.log(value.toUpperCase())
 */
 
 
+function generics <T>(args:T){
+  return args
+}
+
+
+let output1=generics<string>("ROhan")
+console.log(output1)
+
+let output2=generics<number>(199)
+console.log(output2)
