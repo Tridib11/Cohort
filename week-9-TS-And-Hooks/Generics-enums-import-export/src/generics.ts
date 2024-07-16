af3a99175ee27c60c278cf4cccb1a0455beb89a0
Tridib11 <tridibghosh12345@gmail.com>
2024-07-16 16:34:26 +0530

    feat: Add generic function for retrieving the first element of an array

---
+++ week-9-TS-And-Hooks/Generics-enums-import-export/src/generics.ts
@@ -0,0 +1,14 @@
+/*
+can do this in js but not in ts
+
+type input=number|string
+function Fname(name:input[]):input{
+  return name[0]
+}
+
+
+const value=Fname(["Rohan","Paul"])
+
+console.log(value.toUpperCase())
+
+*/
