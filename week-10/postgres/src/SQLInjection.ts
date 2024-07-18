f67e4e94593e06e951ea0d0a6afc2d2f025fe017
Tridib11 <tridibghosh12345@gmail.com>
2024-07-19 00:54:15 +0530

    chore: Refactor code and update PostgreSQL connection setup

---
+++ week-10/postgres/src/SQLInjection.ts
@@ -0,0 +1,23 @@
+import { Client } from "pg";
+const client=new Client({
+  connectionString:"postgresql://cohort_owner:dHn9z4ToxuaG@ep-wild-unit-a5tgec3a.us-east-2.aws.neon.tech/cohort?sslmode=require"
+})
+
+async function insertData(username: string, email: string, password: string) {
+  await client.connect()
+
+  try {
+    await client.connect(); // Ensure client connection is established
+    const insertQuery=`INSERT INTO users (username,email,password) VALUES($1,$2,$3)`;
+    const values=[username,email,password]  
+    const res=await client.query(insertQuery,values)
+    console.log('Insertion success:', res); // Output insertion result
+  } catch (err) {
+    console.error('Error during the insertion:', err);
+  } finally {
+    await client.end(); // Close the client connection
+  }
+}
+
+// Example usage
+insertData('username5', 'user5@example.com', 'user_password').catch(console.error);
