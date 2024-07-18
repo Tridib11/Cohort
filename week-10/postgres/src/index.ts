1ae44f533c11c088b8137ce2c7179a6b020e2d56
Tridib11 <tridibghosh12345@gmail.com>
2024-07-19 00:15:55 +0530

    chore: Remove unused code and update PostgreSQL connection setup

--- week-10/postgres/src/index.ts
+++ week-10/postgres/src/index.ts
@@ -3,4 +3,21 @@ const client=new Client({
   connectionString:"postgresql://cohort_owner:dHn9z4ToxuaG@ep-wild-unit-a5tgec3a.us-east-2.aws.neon.tech/cohort?sslmode=require"
 })
 
-client.connect()
+
+async function createUsersTable(){
+  await client.connect()
+  const result=await client.query(`
+          CREATE TABLE users (
+          id SERIAL PRIMARY KEY,
+          username VARCHAR(50) UNIQUE NOT NULL,
+          email VARCHAR(255) UNIQUE NOT NULL,
+          password VARCHAR(255) NOT NULL,
+          created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+      );    
+    `)
+    console.log(result)
+
+}
+
+
+createUsersTable()
